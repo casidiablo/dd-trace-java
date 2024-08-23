ae77c14da8ab486569de0f06f8c450e584bf43b2
Nikita Tkachenko <nikita.tkachenko@datadoghq.com>
2024-08-23 14:53:48 +0200

    Remove FIXME comment

--- internal-api/src/main/java/datadog/trace/api/Config.java
+++ internal-api/src/main/java/datadog/trace/api/Config.java
@@ -1812,7 +1812,6 @@ public class Config {
     ciVisibilityCodeCoverageEnabled =
         configProvider.getBoolean(CIVISIBILITY_CODE_COVERAGE_ENABLED, true);
     // FIXME nikita: should be set to true in ExecutionSettings if Jacoco detected?
-    // FIXME nikita: see if test environment needs updating after changing config property name
     ciVisibilityCoverageLinesEnabled =
         configProvider.getBoolean(CIVISIBILITY_CODE_COVERAGE_LINES_ENABLED, false);
     ciVisibilityCodeCoverageReportDumpDir =
